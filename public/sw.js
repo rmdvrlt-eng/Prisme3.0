5883077e67882768b1cfe4af3072f59045a13189
rmdvrlt-eng <rmdvrlt@gmail.com>
2026-07-14 13:22:07 +0200

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+// Prisme: service worker neutralisé pour éviter de conserver une ancienne interface.
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))).then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", () => {});
